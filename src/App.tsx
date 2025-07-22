9226753d42bfce972a2c0d428708bf6ae10a85c7
TodTete <131738372+TodTete@users.noreply.github.com>
2025-07-22 14:53:58 -0600

    Descripción del cambio

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Index />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
